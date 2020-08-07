d63f51fa51234d7ae616a84d9e0115bc0109e427
Nellie Tobey <35578867+nelliesnoodles@users.noreply.github.com>
2020-08-07 18:18:15 -0400

    adding speech to text
    first test run.

--- reader.js
+++ reader.js
@@ -1,6 +1,10 @@
 // JavaScript source code for speech experiment
 
 //const speech_div;
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+let recognition = new SpeechRecognition();
 
 var current_index = 0;
 var max_len = Monty_text.length - 1;
@@ -13,17 +17,47 @@ function get_line() {
     else {
         current_index += 1;
     }
-    return line;   
-  
+    return line;
+
 };
 
+function startREC(){
+  recognition.start()
+}
 
+
+
+recognition.onresult = function(event) {
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The first [0] returns the SpeechRecognitionResult at the last position.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The second [0] returns the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object
+  let textELEM = document.getElementById('sp2txt')
+  let text = event.results[0][0].transcript;
+  textELEM.innerHTML = text
+}
+
+recognition.onspeechend = function() {
+  recognition.stop();
+}
+
+
+
+recognition.onerror = function(event) {
+  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
 
 
 function addEventlistener_topage() {
     console.log("adding event listeners");
     element = document.getElementById("button1");
     element.addEventListener("click", speak);
+    sp2txt = document.getElementById('sp2txt_button')
+    sp2txt.addEventListener('click', startREC)
 }
 
 function speak() {
@@ -33,9 +67,3 @@ function speak() {
 
     speechSynthesis.speak(new SpeechSynthesisUtterance(line));
 }
-speak();
-
-
-
-
-    
